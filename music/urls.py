54cacca130d566f8da60776cbf9c8993de17dba0
SSep <ssep4u@gmail.com>
2017-03-01 10:47:32 +0900

    24. Favorite View Function 21. Namespace and HTTP 404 Shortcut

--- music/urls.py
+++ music/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
     # /music/<album.id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/<album.id>/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,11 +1,8 @@
-from django.http import Http404
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 
 def index(request):
-    # return HttpResponse("<h1>This is the Music</h1>")
     all_albums = Album.objects.all()
     context = {
         'all_albums': all_albums,
@@ -15,10 +12,21 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {
         'album': album,
     })
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html',
+                      {'album': album,
+                       'error_message': "You did not select a valid song", })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album, })
