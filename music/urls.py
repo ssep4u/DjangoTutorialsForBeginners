b16cdde06f25b5f2540e3e925959b3e27d5d420b
SSep <ssep4u@gmail.com>
2017-03-01 09:54:28 +0900

    20. Removing Hardcoded URLs

--- music/urls.py
+++ music/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
-    # /music/71/
+    # /music/<album.id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
 ]
