b23bdc86be37f8f7bbadc56d1d03bc147fb3fb29
SSep <ssep4u@gmail.com>
2017-03-04 10:53:07 +0900

    34. User Registration 35. User Model and Creating Accounts 36. User Authentication and Login

--- music/urls.py
+++ music/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(), name='index'),
 
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     # /music/<album.id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
